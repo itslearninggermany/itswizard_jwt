8464f4b2b5a44a6998c6926205e78342162d944f
nordmanii <thomas.nordmann@itslearning.com>
2020-04-08 10:31:33 +0200

    debug

--- refreshToken.go
+++ refreshToken.go
@@ -4,6 +4,7 @@ import (
 	"github.com/jinzhu/gorm"
 	uuid "github.com/satori/go.uuid"
 	"net/http"
+	"time"
 )
 
 type RefreshToken struct {
@@ -31,4 +32,29 @@ func (p *RefreshToken) StoreInDatatbae(dbWebserver *gorm.DB) error {
 func Authentificate(r *http.Request) string {
 
 	return ""
+}
+
+func getRefreshTokenFromDatabase(username string, dbWebserver *gorm.DB) (*RefreshToken, error) {
+	var rToken RefreshToken
+	err := dbWebserver.Where("username = ?", username).Last(&rToken).Error
+	return &rToken, err
+}
+
+func (p *RefreshToken) Valid(dbWebserver *gorm.DB) bool {
+	// Check if Token is in Database:
+	exist := dbWebserver.Where("refresh_token", p.RefreshToken).RecordNotFound()
+	if exist {
+		if getHoursScinceCreatet(p.CreatedAt) > 0 {
+			return false
+		} else {
+			return true
+		}
+	} else {
+		return false
+	}
+}
+
+func getHoursScinceCreatet(input time.Time) float64 {
+	aus := time.Now().Sub(input)
+	return aus.Minutes()
 }
