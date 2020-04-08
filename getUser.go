7f938678dd433612b1132ac678ed3fc3e1d08dbb
nordmanii <thomas.nordmann@itslearning.com>
2020-04-09 00:23:03 +0200

    debug

--- getUser.go
+++ getUser.go
@@ -2,41 +2,54 @@ package itswizard_jwt
 
 import (
 	"fmt"
-	"github.com/itslearninggermany/itszwizard_objects"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/jinzhu/gorm"
 	"net/http"
 )
 
-func GetUser(r *http.Request, dbWebserver *gorm.DB) (user itszwizard_objects.SessionUser, err error) {
+func GetUser(r *http.Request, dbWebserver *gorm.DB) (user jwt.MapClaims, err error) {
 	auth, err := DecodeAuthentification(r, dbWebserver)
 	if err != nil {
 		return user, err
 	}
 	fmt.Println(auth.IDToken)
 
-	jwtData, err := decodePayload(auth.IDToken)
+	claims := jwt.MapClaims{}
+	_, err = jwt.ParseWithClaims(auth.IDToken, claims, func(token *jwt.Token) (interface{}, error) {
+		return []byte(GetAuthKeys(dbWebserver).GetKey()), nil
+	})
 	if err != nil {
 		return user, err
 	}
 
-	user = itszwizard_objects.SessionUser{
-		Username:            jwtData.Username,
-		UserID:              jwtData.UserID,
-		FirstAuthentication: jwtData.FirstAuthentication,
-		Authenticated:       jwtData.Authenticated,
-		TwoFac:              jwtData.TwoFac,
-		Firstname:           jwtData.Firstname,
-		Lastname:            jwtData.Lastname,
-		Mobile:              jwtData.Mobile,
-		IpAddress:           jwtData.IPAddress,
-		Institution:         jwtData.Institution,
-		School:              jwtData.School,
-		Email:               jwtData.Email,
-		Information:         jwtData.Information,
-		Admin:               jwtData.Admin,
-		OrganisationID:      jwtData.OrganisationID,
-		InstitutionID:       jwtData.InstitutionID,
-	}
+	return claims, err
 
-	return
+	/*
+
+		jwtData, err := decodePayload(auth.IDToken)
+		if err != nil {
+			return user, err
+		}
+
+		user = itszwizard_objects.SessionUser{
+			Username:            jwtData.Username,
+			UserID:              jwtData.UserID,
+			FirstAuthentication: jwtData.FirstAuthentication,
+			Authenticated:       jwtData.Authenticated,
+			TwoFac:              jwtData.TwoFac,
+			Firstname:           jwtData.Firstname,
+			Lastname:            jwtData.Lastname,
+			Mobile:              jwtData.Mobile,
+			IpAddress:           jwtData.IPAddress,
+			Institution:         jwtData.Institution,
+			School:              jwtData.School,
+			Email:               jwtData.Email,
+			Information:         jwtData.Information,
+			Admin:               jwtData.Admin,
+			OrganisationID:      jwtData.OrganisationID,
+			InstitutionID:       jwtData.InstitutionID,
+		}
+
+		return
+	*/
 }
