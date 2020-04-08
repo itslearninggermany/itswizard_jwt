54933845f66879db707ffb13216cc9792429280a
nordmanii <thomas.nordmann@itslearning.com>
2020-04-09 00:43:24 +0200

    debug

--- getUser.go
+++ getUser.go
@@ -3,11 +3,12 @@ package itswizard_jwt
 import (
 	"fmt"
 	"github.com/dgrijalva/jwt-go"
+	"github.com/itslearninggermany/itszwizard_objects"
 	"github.com/jinzhu/gorm"
 	"net/http"
 )
 
-func GetUser(r *http.Request, dbWebserver *gorm.DB) (user jwt.MapClaims, err error) {
+func GetUser(r *http.Request, dbWebserver *gorm.DB) (user itszwizard_objects.SessionUser, err error) {
 	auth, err := DecodeAuthentification(r, dbWebserver)
 	if err != nil {
 		return user, err
@@ -22,34 +23,25 @@ func GetUser(r *http.Request, dbWebserver *gorm.DB) (user jwt.MapClaims, err err
 		return user, err
 	}
 
-	return claims, err
+	user = itszwizard_objects.SessionUser{
+		Username:            claims["Username"].(string),
+		UserID:              claims["UserID"].(uint),
+		FirstAuthentication: claims["FirstAuthentication"].(bool),
+		Authenticated:       claims["Authenticated"].(bool),
+		TwoFac:              claims["TwoFac"].(bool),
+		Firstname:           claims["Firstname"].(string),
+		Lastname:            claims["Lastname"].(string),
+		Mobile:              claims["Mobile"].(string),
+		IpAddress:           claims["IPAddress"].(string),
+		Institution:         claims["Institution"].(string),
+		School:              claims["School"].(string),
+		Email:               claims["Email"].(string),
+		Information:         claims["Information"].(string),
+		Admin:               claims["Admin"].(bool),
+		OrganisationID:      claims["OrganisationID"].(uint),
+		InstitutionID:       claims["InstitutionID"].(uint),
+	}
 
-	/*
+	return
 
-		jwtData, err := decodePayload(auth.IDToken)
-		if err != nil {
-			return user, err
-		}
-
-		user = itszwizard_objects.SessionUser{
-			Username:            jwtData.Username,
-			UserID:              jwtData.UserID,
-			FirstAuthentication: jwtData.FirstAuthentication,
-			Authenticated:       jwtData.Authenticated,
-			TwoFac:              jwtData.TwoFac,
-			Firstname:           jwtData.Firstname,
-			Lastname:            jwtData.Lastname,
-			Mobile:              jwtData.Mobile,
-			IpAddress:           jwtData.IPAddress,
-			Institution:         jwtData.Institution,
-			School:              jwtData.School,
-			Email:               jwtData.Email,
-			Information:         jwtData.Information,
-			Admin:               jwtData.Admin,
-			OrganisationID:      jwtData.OrganisationID,
-			InstitutionID:       jwtData.InstitutionID,
-		}
-
-		return
-	*/
 }
