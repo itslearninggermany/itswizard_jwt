1a5f55f8b90a6b9cdb7fd4253d8e4f9b5c745231
nordmanii <thomas.nordmann@itslearning.com>
2020-04-13 20:54:59 +0200

    debug

--- createToken.go
+++ createToken.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/dgrijalva/jwt-go"
 	"github.com/itslearninggermany/itswizard_basic"
+	"github.com/itslearninggermany/itszwizard_objects"
 	"github.com/jinzhu/gorm"
 	"net/http"
 	"strings"
@@ -16,14 +17,13 @@ type JwtSession struct {
 	Token    string `gorm:"type:MEDIUMTEXT"`
 }
 
-func CreateToken(r *http.Request, username string, dbUser *gorm.DB, dbWebserver *gorm.DB) string {
+func CreateToken(r *http.Request, username string, dbUser *gorm.DB, dbWebserver *gorm.DB) (authString string, sessionUser itszwizard_objects.SessionUser, err error) {
 
 	var user itswizard_basic.DbItswizardUser15
 	err := dbUser.Where("username = ?", username).First(&user).Error
 	if err != nil {
-		// TODO HTTP REDIRECT
 		fmt.Println(err)
-		return ""
+		return "", sessionUser, err
 	}
 
 	var orga itswizard_basic.DbOrganisation15
@@ -31,7 +31,7 @@ func CreateToken(r *http.Request, username string, dbUser *gorm.DB, dbWebserver 
 	if err != nil {
 		// TODO HTTP REDIRECT
 		fmt.Println(err)
-		return ""
+		return "", sessionUser, err
 	}
 
 	var inst itswizard_basic.DbInstitution15
@@ -39,7 +39,7 @@ func CreateToken(r *http.Request, username string, dbUser *gorm.DB, dbWebserver 
 	if err != nil {
 		// TODO HTTP REDIRECT
 		fmt.Println(err)
-		return ""
+		return "", sessionUser, err
 	}
 
 	ip := strings.Split(r.RemoteAddr, ":")
@@ -65,11 +65,30 @@ func CreateToken(r *http.Request, username string, dbUser *gorm.DB, dbWebserver 
 		"iat":                 time.Now().Unix(),
 	})
 
+	sessionUser = itszwizard_objects.SessionUser{
+		Username:            username,
+		UserID:              user.Model.ID,
+		FirstAuthentication: true,
+		Authenticated:       true,
+		TwoFac:              user.TwoFac,
+		Firstname:           user.Firstname,
+		Lastname:            user.Lastname,
+		Mobile:              user.Tel,
+		IpAddress:           ip[0],
+		Institution:         inst.Name,
+		School:              orga.Name,
+		Email:               user.Email,
+		Information:         "--",
+		Admin:               false,
+		OrganisationID:      orga.ID,
+		InstitutionID:       inst.ID,
+	}
+
 	tokenString, err := token.SignedString(GetAuthKeys(dbWebserver).GetKey())
 	if err != nil {
 		// TODO HTTP REDIRECT
 		fmt.Println(err)
-		return ""
+		return "", sessionUser, err
 	}
 
 	refreshToken := createRefreshToken(username)
@@ -77,7 +96,7 @@ func CreateToken(r *http.Request, username string, dbUser *gorm.DB, dbWebserver 
 	if err != nil {
 		// TODO HTTP REDIRECT
 		fmt.Println(err)
-		return ""
+		return "", sessionUser, err
 	}
 
 	//Store And logout
@@ -93,8 +112,8 @@ func CreateToken(r *http.Request, username string, dbUser *gorm.DB, dbWebserver 
 	if err != nil {
 		// TODO HTTP REDIRECT
 		fmt.Println(err)
-		return ""
+		return "", sessionUser, err
 	}
 
-	return CreateNewAuthUrl("123", tokenString, refreshToken.String(), dbWebserver)
+	return CreateNewAuthUrl("123", tokenString, refreshToken.String(), dbWebserver), sessionUser, nil
 }
