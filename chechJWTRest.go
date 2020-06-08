e441132c9c03b8926ca8eb33eff03e28b1c58d22
nordmanii <thomas.nordmann@itslearning.com>
2020-06-08 22:37:40 +0200

    add rest auth

---
+++ chechJWTRest.go
@@ -0,0 +1,31 @@
+package itswizard_jwt
+
+import (
+	"encoding/json"
+	jwtmiddleware "github.com/auth0/go-jwt-middleware"
+	"github.com/dgrijalva/jwt-go"
+	"github.com/itslearninggermany/itswizard_jwt"
+	"github.com/itslearninggermany/uploadrest"
+	"github.com/jinzhu/gorm"
+	"net/http"
+)
+
+func CheckJWTRest(w http.ResponseWriter, r *http.Request, dbwebserver *gorm.DB) error {
+	jwtMiddleware := jwtmiddleware.New(jwtmiddleware.Options{
+		ValidationKeyGetter: func(token *jwt.Token) (interface{}, error) {
+			return itswizard_jwt.GetAuthKeys(dbwebserver).GetKey(), nil
+		},
+		SigningMethod: jwt.SigningMethodHS256,
+		Extractor: func(r *http.Request) (string, error) {
+			tmp, err := uploadrest.Decrypt(itswizard_jwt.GetAuthKeys(dbwebserver).GetAes(), r.Header.Get("Authorization"))
+			var auth itswizard_jwt.Authentication
+			err = json.Unmarshal([]byte(tmp), &auth)
+			return auth.IDToken, err
+		},
+		ErrorHandler: func(w http.ResponseWriter, r *http.Request, err string) {
+			http.Error(w, err, http.StatusUnauthorized)
+		},
+	})
+
+	return jwtMiddleware.CheckJWT(w, r)
+}
